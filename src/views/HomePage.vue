f80b66b88132669dee333cc1da5244015ebe4efc
comeP <come.picard@gmail.com>
2023-06-15 23:00:36 +0200

    :sparkles: FEAT HomePage:
    Création de la vue de HomePage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <HeaderNavBar></HeaderNavBar>
+</template>
+
+<script>
+
+import HeaderNavBar from "@/components/HeaderNavBar.vue";
+export default {
+  name: "HomePage",
+
+  components: {
+    HeaderNavBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
