4eb25aeded15a4b6a1124251521b58c97b275aed
comeP <come.picard@gmail.com>
2023-06-15 21:16:10 +0200

    :tada: init project

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,44 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+const darkTheme = {
+  dark: true,
+  colors: {
+    background: '#272727',
+    surface: '#575757',
+    primary: '#81BEF8',
+    secondary: '#FFCACA',
+  }
+}
+
+const lightTheme = {
+  dark: false,
+}
+
+export default createVuetify({
+  theme: {
+    // themes: {
+    //   light: {
+    //     colors: {
+    //       primary: '#1867C0',
+    //       secondary: '#5CBBF6',
+    //     },
+    //   },
+    defaultTheme: 'darkTheme',
+    themes: {
+      darkTheme
+    },
+  },
+})
