e93dee3120f88903318a2c5a22d386fd2b521252
ComePicard <come.picard@gmail.com>
2023-06-22 17:03:03 +0200

    Add Article in HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,18 @@
 <template>
   <HeaderNavBar></HeaderNavBar>
+  <ArticleCard desc=""></ArticleCard>
 </template>
 
 <script>
 
 import HeaderNavBar from "@/components/HeaderNavBar.vue";
+import ArticleCard from "@/components/ArticleCard.vue";
 export default {
   name: "HomePage",
 
   components: {
-    HeaderNavBar
+    HeaderNavBar,
+    ArticleCard
   }
 }
 </script>
